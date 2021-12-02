8311dedfe8fe01f179ce128d0bfb97332babd40f
jdjebi <39745200+jdjebi@users.noreply.github.com>
2021-12-02 14:47:47 +0000

    Mise à jour

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -3,15 +3,22 @@ namespace App\Http\Middleware;
 
 use Closure;
 
+use Illuminate\Support\Facades\Log;
+
 class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
         
-        $url =$request->url();
+        $url = $request->url();
         
         if(env("APP_ENV") == "production"){
             $result = explode("://",$url);
+
+            Log::emergency($result);
+            Log::error($result);
+            Log::debug($result);
+
             if($result[0] == "http"){
                 $secure_url = "https://" . $result[1];
                 return redirect($secure_url);
